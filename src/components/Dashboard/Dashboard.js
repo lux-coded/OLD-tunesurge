851e84cb0435a7ace2174995021dc98e957cb850
lux-coded <selakluka@gmail.com>
2021-05-22 23:53:55 +0200

    User data called, basic dashboard styling.

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,23 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { connect, useSelector } from 'react-redux';
 import useAuth from '../useAuth.js';
+// import { getAccessToken } from '../../actions/authActions.js';
 
 import './Dashboard.scss';
 
 const Dashboard = () => {
-  // const [ accessToken, setAccessToken ] = useState();
-  const loginCode = useSelector(state => state.getLoginCode);
+  const [ displayName, setDisplayName ] = useState();
+  const [ avatar, setAvatar ] = useState();
+  const loginCode = useSelector( state => state.getLoginCode );
   // console.log(loginCode);
-  const accessToken = useAuth(loginCode);
-  console.log(accessToken);
+  // setAccessToken(useAuth(loginCode));
+  // console.log(accessToken);
+  const accessToken = useAuth( loginCode );
+
+  useEffect(() => {
+    if (!accessToken) return;
+
+    axios('https://api.spotify.com/v1/me', {
+      // method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    })
+    .then((res) => {
+      console.log(res);
+      setDisplayName(res.data.display_name);
+      setAvatar(res.data.images[0].url);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }, [ accessToken ]);
+
+  // useEffect(() => {
+  //   this.props.getAccessToken(accessToken);
+  // }, [accessToken])
 
   return (
-    <section id='dashboard'>
-      <h1>Dashboard</h1>
-      <div>
-        <h3>My Profile</h3>
+    <section id='dashboard-container'>
+      <div id='dashboard-sidebar' className='dashboard-card'>
+        <div id='dashboard-profile'>
+          <img src={avatar} alt='avatar'></img>
+          <h1>{displayName}</h1>
+        </div>
+        <div id='dashboard-controls'>
+          <h3>My Profile</h3>
+          <h3>Favorites</h3>
+          <h3>Recommended</h3>
+        </div>
       </div>
-      <div>
+      <div id='dashboard-panel' className='dashboard-card'>
         <h3>Library</h3>
       </div>
     </section>
