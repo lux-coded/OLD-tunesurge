3828ec1eccee825f52279ca8933c1b1cf649f4fe
lux-coded <selakluka@gmail.com>
2021-05-24 11:05:51 +0200

    Separate initial auth values.

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,9 +1,12 @@
 import { GET_LOGIN_CODE } from '../actions/types.js';
 import { GET_ACCESS_TOKEN } from '../actions/types.js';
 
-const initialState = '';
+const initialCode = '';
+if (localStorage.getItem('accessToken')) {
+  const initialToken = localStorage.getItem('accessToken');
+}
 
-export const getLoginCode = (state = initialState, action) => {
+export const getLoginCode = (state = initialCode, action) => {
   switch (action.type) {
     case GET_LOGIN_CODE:
       return action.payload
@@ -12,7 +15,7 @@ export const getLoginCode = (state = initialState, action) => {
   }
 };
 
-export const getAccessToken = (state = initialState, action) => {
+export const getAccessToken = (state = initialToken, action) => {
   switch (action.type) {
     case GET_ACCESS_TOKEN:
       return action.payload
--- src/reducers/getSearchResults.js
+++ src/reducers/getSearchResults.js
@@ -5,7 +5,7 @@ const initialState = {};
 const getSearchResults = (state = initialState, action) => {
   switch(action.type) {
     case GET_SEARCH_RESULTS:
-      console.log(action.payload);
+      // console.log(action.payload);
       return {
         ...state,
         ...action.payload
