4e292f059acd1aaf1aaa17b2ea95eb584748388e
lux-coded <selakluka@gmail.com>
2021-05-31 21:33:04 +0200

    Redux persist setup.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 import store from './store.js';
 import './styles/index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+      <App />
   </Provider>,
 document.querySelector('#root'));
--- src/store.js
+++ src/store.js
@@ -1,11 +1,23 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+// import { persistStore, persistReducer } from 'module';
+// import storage from "redux-persist/lib/storage";
 import rootReducer from './reducers/rootReducer';
 import thunk from 'redux-thunk';
+//
+// const persistConfig = {
+//   key: 'root',
+//   storage
+// }
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleware = [thunk];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
+
+// export let store = createStore(persistedReducer, composeEnhancers(applyMiddleware(...middleware)));
+// export let persistor = persistStore(store);
 
 export default store;
