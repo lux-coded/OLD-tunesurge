22d7a837c59ea4718bea5fcd1f02b1e820e49e47
lux-coded <selakluka@gmail.com>
2021-05-31 22:33:50 +0200

    Netlify AUTH_URL hotfix.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Login.scss';
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=7ed88be146d5439d8458b6c9ee0adad4&show_dialog=true&response_type=code&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}/&scope=streaming%20user-read-email%20user-top-read%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=7ed88be146d5439d8458b6c9ee0adad4&show_dialog=true&response_type=code&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=streaming%20user-read-email%20user-top-read%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
 class Login extends React.Component {
 
